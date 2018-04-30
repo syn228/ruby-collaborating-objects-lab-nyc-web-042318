329d7c021511df3ce18a2c3dfd5184513ec8c006
Aaron Na <syn228@nyu.edu>
2018-04-30 21:13:07 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -8,11 +8,7 @@ class Song
   end
   
   def self.new_by_filename(filename)
-    x = filename.split(" - ")
-    x[2].slice!(".mp3")
-    Song.new(x[1])
-    self.artist.name = x[0]
-    self.name = x[1]
+    fileame
     binding.pry
   end
   
