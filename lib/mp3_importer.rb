e00ef3c63356eefbf571c782598a7d88b673daf1
Aaron Na <syn228@nyu.edu>
2018-04-30 20:30:46 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,7 +1,11 @@
 class MP3Importer
   
   def files
-  
+    Dir.foreach(@path) do |file_name|
+      if file_name.end_with? "mp3"
+        @files << file_name
+      end
+    end
   end
   
   def import
