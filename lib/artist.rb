d5262c33a8454dcdd6a7ad1ff26bf230ae4c6420
Aaron Na <syn228@nyu.edu>
2018-04-30 20:30:01 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,9 +1,16 @@
 class Artist
   
-  attr_accessor :name
+  attr_accessor :name, :songs
+  
+  @@songs = []
   
   def initialize(name)
     @name = name
   end
+  
+  def self.save
+    @@all
+  end
+  
   
 end
